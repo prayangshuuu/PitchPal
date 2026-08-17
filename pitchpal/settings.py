a0b1ab10efd316087d01be052300e50641e8bdd5
Prayangshu <prayangshubiswashritwick@gmail.com>
2026-08-17 16:04:25 +0600

    fix: override shell environment variables with local .env

--- pitchpal/settings.py
+++ pitchpal/settings.py
@@ -15,7 +15,7 @@ import os
 import dj_database_url
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(override=True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
